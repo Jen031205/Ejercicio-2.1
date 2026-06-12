623a426b01c608ef0b9ae003660fcec23dc53af8
Jennifer Ailin Medina Hernandez <jenniferailin031205@gmail.com>
2026-06-12 01:41:54 -0600

    feat: add WearAlertaScreen and SwipeDismissableNavHost navigation

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -63,4 +63,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-guava:1.7.3")
 // nuevoo
 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
+    implementation("androidx.navigation:navigation-compose:2.8.0")
+    implementation("androidx.wear.compose:compose-navigation:1.4.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+
 }
