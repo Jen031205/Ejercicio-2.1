b4a383f647d3b23e85bccfc70e542ec05b34d5a0
Jennifer Ailin Medina Hernandez <jenniferailin031205@gmail.com>
2026-07-13 14:54:47 -0600

    Commit: feat(cast): add Cast SDK dependencies to app module

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -59,4 +59,10 @@ dependencies {
     implementation("androidx.media3:media3-exoplayer:1.4.1")
     implementation("androidx.media3:media3-ui:1.4.1")
     implementation("androidx.media3:media3-common:1.4.1")
+
+    // Cast SDK
+    implementation("androidx.mediarouter:mediarouter:1.7.0")
+    implementation("com.google.android.gms:play-services-cast-framework:21.5.0")
+
+
 }
